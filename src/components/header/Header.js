a300376ff8ee5164d1a715df7b16ff6a9ab060c4
Ahmad <ahmad.zahedi121288@gmail.com>
2023-11-27 15:22:08 +0330

    feat(Header): create new component

---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+export default function Header() {
+
+    return(
+        <header className='w-100 h-32 bg-slate-300'>
+            <ul>
+                <li>menu</li>
+                <li>menu</li>
+                <li>menu</li>
+                <li>menu</li>
+            </ul>
+        </header>
+    )
+}
