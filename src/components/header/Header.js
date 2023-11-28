f09ef033a6139b8f69a591c59a4d16e7cda90068
Ahmad Zahedi <ahmad.zahedi121288@gmail.com>
2023-11-28 21:23:54 +0330

    feat(Header): create html of header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,3 +1,11 @@
+// Next
+import Image from 'next/image';
+// Icons
+import SearchIcon from '@/icons/search.svg';
+import ShopIcon from '@/icons/shop.svg';
+import Profile from '@/icons/profile.svg';
+import ArrowDown from '@/icons/arrow-down.svg';
+
 export default function Header() {
     const menuItems = [
         {
@@ -17,10 +25,71 @@ export default function Header() {
             children: []
         }
     ];
-    return(
-        <header className="w-100 py-6 px-24 bg-slate-300">
-            <div className="font-bold text-4xl">SHOP.CO</div>
 
+    return (
+        <header className="w-100 py-6 px-24">
+            <div className="flex gap-10 items-center justify-between">
+                <div className="font-bold text-4xl">SHOP.CO</div>
+
+                <ul className="flex gap-6 align-middle">
+                    {
+                        menuItems.map((item, index) => (
+                            <div
+                                className="flex gap-1 cursor-pointer"
+                                key={index}
+                            >
+                                <li>
+                                    {item.label}
+                                    {item.children.length > 0 && (
+                                        <ul className="hidden">
+                                            {
+                                                item.children.map((child, childIndex) => (
+                                                    <li key={childIndex}>{child}</li>
+                                                ))
+                                            }
+                                        </ul>
+                                    )}
+                                </li>
+                                {item.children.length > 0 &&
+                                    <Image
+                                        src={ArrowDown}
+                                        alt="arrow icon"
+                                    />
+                                }
+                            </div>
+                        ))
+                    }
+                </ul>
+
+                <div className="bg-gray-100 px-4 py-3 rounded-full flex gap-3 grow">
+                    <Image
+                        src={SearchIcon}
+                        alt="search icon"
+                    />
+
+                    <input
+                        className="w-full border-none bg-transparent focus:outline-none"
+                        type="text"
+                        placeholder="Search for products..."
+                    />
+                </div>
+
+                <div className="flex gap-3.5">
+                    <button>
+                        <Image
+                            src={ShopIcon}
+                            alt="shop icon"
+                        />
+                    </button>
+
+                    <button>
+                        <Image
+                            src={Profile}
+                            alt="profile icon"
+                        />
+                    </button>
+                </div>
+            </div>
         </header>
-    )
+    );
 }
