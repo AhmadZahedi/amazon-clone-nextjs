2cc0e96347f40020bd1b6ac820181de95e2ad2c7
Ahmad Zahedi <ahmad.zahedi121288@gmail.com>
2023-12-03 00:09:28 +0330

    test card usage

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,21 @@
+import CardComponent from "@/components/card.component";
 
 export default function Main() {
+    const product = {
+        name: 'Skinny Fit Jeans',
+        rating: 3,
+        price: 260,
+        discountPercent: 20
+    };
+
     return (
         <>
             <div className='text-[64px]'>
                 FIND CLOTHES THAT MATCHES YOUR STYLE
+                <div className="flex w-full h-[500px] justify-center items-center">
+                    <CardComponent width={250} alt={'alt'} product={product}/>
+                    <CardComponent width={250} alt={'alt'} product={product}/>
+                </div>
             </div>
         </>
     );
