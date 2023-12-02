30a83e09e96f4150923e6daedc9abb678db2ddcb
Ahmad Zahedi <ahmad.zahedi121288@gmail.com>
2023-12-02 19:14:05 +0330

    refactor(header): use new menu component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,86 +6,21 @@ import Image from 'next/image';
 import ShopIcon from '@/icons/shop.svg';
 
 // Ant Design
-import {DownOutlined} from '@ant-design/icons';
-import {Menu, Avatar, Badge, Space} from 'antd';
-
-// React
-import {useState} from "react";
+import {Avatar, Badge, Space} from 'antd';
 
 // Components
-import SearchBar from "@/components/header/searchBar";
-import UserActions from "@/components/header/userActions";
+import SearchBar from "@/components/header/searchBar.comopnent";
+import UserActions from "@/components/header/userActions.component";
+import MenuComponent from "@/components/header/menu.component";
 
 export default function Header() {
-
-    const items = [
-        {
-            label: 'Shop',
-            key: 'shop',
-            icon: <DownOutlined/>,
-            children: [
-                {
-                    type: 'group',
-                    label: 'Men Clothes',
-                    children: [
-                        {
-                            label: 'Jeans',
-                            key: 'men jeans'
-                        },
-                        {
-                            label: 'Shirts',
-                            key: 'men shirts'
-                        }
-                    ]
-                },
-                {
-                    type: 'group',
-                    label: 'Women Clothes',
-                    children: [
-                        {
-                            label: 'Jeans',
-                            key: 'women jeans'
-                        },
-                        {
-                            label: 'Shirts',
-                            key: 'women shirts'
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            label: 'On Sale',
-            key: 'on sale'
-        },
-        {
-            label: 'New Arrivals',
-            key: 'new arrivals'
-        },
-        {
-            label: 'Brands',
-            key: 'brands'
-        }
-    ];
-
-    const [currentMenu, setCurrentMenu] = useState('men jeans');
-
-    function changeMenu(e) {
-        setCurrentMenu(e.key)
-    }
-
     return (
         <header className="w-100 py-6 px-24">
             <div className="flex gap-10 items-center justify-between">
                 <button className="font-bold text-4xl font-integralCf">SHOP.CO</button>
 
                 <div className="w-[380px]">
-                    <Menu
-                        mode="horizontal"
-                        items={items}
-                        selectedKeys={[currentMenu]}
-                        onClick={changeMenu}
-                    />
+                    <MenuComponent />
                 </div>
 
                 <SearchBar />
